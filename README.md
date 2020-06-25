c375e8d413c766efec91d0a66d3a9e950c83cb6d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-25 01:44:05 +0000

    Deploy Hunter-Gu/snake-game to github.com/Hunter-Gu/snake-game.git:gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# snake-game
-wip
---
+++ precache-manifest.9fb2e8b4003a85a44c9f8ef7bb17757c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a557f0cb9c2c9e8b1a",
+    "url": "/snake-game/css/app.0fa2fe07.css"
+  },
+  {
+    "revision": "bd06211f5ff44e9ca3f81511785255af",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "fdc672209a70cb630c07",
+    "url": "/snake-game/js/about.839fec52.js"
+  },
+  {
+    "revision": "72a557f0cb9c2c9e8b1a",
+    "url": "/snake-game/js/app.3c54ed54.js"
+  },
+  {
+    "revision": "4ca59e35ca448e01725c",
+    "url": "/snake-game/js/chunk-vendors.0723abfe.js"
+  },
+  {
+    "revision": "f41e8f85b5553be9d57dc0f14edd2e59",
+    "url": "/snake-game/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/snake-game/robots.txt"
+  }
+]);
